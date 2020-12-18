28f2856d4d879fe3f70540df88f61b8d665e0db1
Josh709-cell <73429343+Josh709-cell@users.noreply.github.com>
2020-12-18 22:26:40 +0000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+const Discord = require('discord.js');
+ const client = new Discord.Client();
+
+client.on('ready', () => {
+ console.log(`Logged in as ${client.user.tag}!`);
+ });
+
+client.on('message', msg => {
+ if (msg.content === 'website') {
+ msg.reply('Website comming soon!');
+ }
+ });
+
+client.login('Token Here');
